f567dd514fd0cf9670ff509560623fdc888dd291
VivekGupta <Vivek.Guptavg@outlook.com>
2024-11-03 17:33:47 -0500

    made visual changes to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,25 +5,33 @@ const Footer = () =>{
     return (
 
             <Container fluid className="footer p-5">
-                <Row className='justify-content-center py-3'>  
-                    <Col className='text-center'><Link to="/" className="my-brand">Sale Spotter</Link></Col>
+                <Row className='py-3'>
                     <Col className="company-tag text-center">Connecting local buyers and sellers with ease!</Col>
-                    <Col className='text-center d-none d-lg-block'><Link to="/about-us" className="page-links">About us</Link></Col>
                 </Row>
 
-                <Row className='py-3'>
-                    <Col className='text-center'>
+                <Row className='py-5'>
+                    <Col>   
+                            <Row><Link to="/" className="my-brand">Sale Spotter</Link></Row>
                             <Row><Link to="/" className="page-links">Home</Link></Row>
+                            <Row><Link to="/post-ad" className="page-links">Post Ad</Link></Row> 
                             <Row><Link to="/ads" className="page-links">Ads</Link></Row>
-                            {/* <Row><Link to="/post-ad" className="page-links">Post Ad</Link></Row> */}
 
-                            
                     </Col>
                     <Col className='text-center'>
-                            <Row><Link to="/contact-us" className="page-links">Contact Us</Link></Row>
+                            <text className="my-brand">Info</text>
+                           
+                            <Row><Link to="/about-us" className="page-links">About Us</Link></Row>
                             <Row><Link to="/privacy-policy" className="page-links">Privacy Policy</Link></Row>
                     </Col>
-                    <Col>@SaleSpotter, Inc 2024. We Love Users.</Col>
+                    <Col className='text-center'>
+                            <text className="my-brand">Contact</text>  
+                            <Row><Link to="/contact-us" className="page-links">Contact Us</Link></Row>
+                    </Col>
+                    
+                </Row>
+                <Row>
+                <Col className='text-center py-3 company-tag'>@SaleSpotter, Inc 2024. We Love Users.</Col>
+              
                 </Row>
             </Container>
 
